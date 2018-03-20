bb25fcca4b63d2eb741dbd6c5511900157f9d6ba
jin00105 <hy324@cornell.edu>
2018-03-21 07:51:43 +0900

    hey

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-from flask import Flask
-#from flask import Flask, render_template, url_for
-#from flask import request, redirect, jsonify, flash
-#import random
-#import string
+from flask import Flask, render_template, url_for
+from flask import request, redirect, jsonify, flash
+import random
+import string
 #import httplib2
-#from flask import make_response
-#import requests
+from flask import make_response
+import requests
 
 app = Flask(__name__)
 
 # main page
-#@app.route('/')
-#@app.route('/winterwings')
-#def main():
-#	return  'Hello World!' #render_template('main.html')
-
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	return "Hello World"
+@app.route('/winterwings')
+def main():
+	return render_template('main.html')
+
+@app.route('/form')
+def form():
+	return render_template('form.html')
 
 if __name__ == '__main__':
-#	app.secret_key = 'super_secret_key'
+	app.secret_key = 'super_secret_key'
 	app.debug = True
 	app.run(host='0.0.0.0', port=5000)
---
+++ templates/Todo.md
@@ -0,0 +1,22 @@
+html
+-link nav bars to corresponding areas of the page
+-link footers
+-cut the footer logos out of the original pic
+-make it into a hamburger icon if it gets too small.
+
+css
+-divide sections in different colors
+-content titles bold & style change.
+js
+- make the headers drop down together
+- make logo fade in
+
+layout:
+how to participate:
+	how does it work
+	protocol 
+		why should i follow the protocol
+		protocol
+			how to watch
+			when to watch
+		Identification
